fd3cb59eae55e929c64d224176c7916a395adca4
abhi192809 <Abhishekkapoor192809@gmail.com>
2024-01-24 14:07:02 +0530

    Today
    Today

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->load->view('dashboard/header');
+        $this->load->view('dashboard/sidebar');
+        $this->load->view('dashboard/topbar');
+        $this->load->view('dashboard/index');
+        $this->load->view('dashboard/footer');
+    }
+}
